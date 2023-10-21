50cc47e074ae65e89bf0201e825549d72c1143ef
Brock Callahan <jbrockc@gmail.com>
2023-10-21 16:34:11 -0500

    add protocol
    specify whether the port is used for UDP, TCP, or both.

--- portn.py
+++ portn.py
@@ -2,31 +2,48 @@
 
 import sys
 
-# Define a dictionary of common services and their ports
+# Define a dictionary of common services, their ports, and protocols
 services = {
-    21: "FTP",
-    22: "SSH",
-    23: "Telnet",
-    25: "SMTP",
-    53: "DNS",
-    80: "HTTP",
-    110: "POP3",
-    119: "NNTP",
-    123: "NTP",
-    143: "IMAP",
-    161: "SNMP",
-    443: "HTTPS",
-    445: "Microsoft-DS",
-    993: "IMAP over SSL",
-    995: "POP3 over SSL",
-    3306: "MySQL",
-    3389: "MS RDP",
-    5900: "VNC",
+    20: ("FTP Data", "TCP"),
+    21: ("FTP Control", "TCP"),
+    22: ("SSH", "TCP"),
+    23: ("Telnet", "TCP"),
+    25: ("SMTP", "TCP"),
+    53: ("DNS", "Both"),
+    67: ("DHCP Server", "UDP"),
+    68: ("DHCP Client", "UDP"),
+    69: ("TFTP", "UDP"),
+    80: ("HTTP", "TCP"),
+    110: ("POP3", "TCP"),
+    119: ("NNTP", "TCP"),
+    123: ("NTP", "UDP"),
+    135: ("MS RPC", "TCP"),
+    137: ("NetBIOS Name Service", "UDP"),
+    138: ("NetBIOS Datagram Service", "UDP"),
+    139: ("NetBIOS Session Service", "TCP"),
+    143: ("IMAP", "TCP"),
+    161: ("SNMP", "UDP"),
+    162: ("SNMP Trap", "UDP"),
+    389: ("LDAP", "TCP"),
+    443: ("HTTPS", "TCP"),
+    445: ("Microsoft-DS", "TCP"),
+    500: ("ISAKMP", "UDP"),
+    636: ("LDAPS", "TCP"),
+    989: ("FTPS Data", "TCP"),
+    990: ("FTPS Control", "TCP"),
+    993: ("IMAP over SSL", "TCP"),
+    995: ("POP3 over SSL", "TCP"),
+    1812: ("RADIUS Authentication", "UDP"),
+    1813: ("RADIUS Accounting", "UDP"),
+    3389: ("MS RDP", "TCP"),
+    5060: ("SIP", "UDP"),
+    5061: ("SIPS", "TCP"),
+    5900: ("VNC", "TCP"),
     # ... Add more as needed
 }
 
 def get_service(port):
-    return services.get(port, "Unknown")
+    return services.get(port, ("Unknown", "Unknown"))
 
 if __name__ == "__main__":
     if len(sys.argv) != 2:
@@ -34,6 +51,6 @@ if __name__ == "__main__":
         sys.exit(1)
 
     port = int(sys.argv[1])
-    service = get_service(port)
-    print(f"Port {port} is commonly used for {service}")
+    service, protocol = get_service(port)
+    print(f"Port {port} is commonly used for {service} over {protocol}")
 
